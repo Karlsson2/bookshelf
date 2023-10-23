e90332f110704db65c0b24c1df4a8cb924bbb0a5
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2023-10-23 18:33:24 +0200

    added functions for sorting the bookshelf

--- index.php
+++ index.php
@@ -2,6 +2,29 @@
 
 require __DIR__ . "/book-array-generated.php";
 require __DIR__ . "/header.php";
+require __DIR__ . "/functions.php";
+
+$sorting = $_GET["sorting"] ?? null;
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (isset($sorting)) {
+        switch ($sorting) {
+            case "alphabetically":
+                $bookArray = sortAlphabetically($bookArray);
+                break;
+            case "size":
+                $bookArray = sortBySize($bookArray);
+                break;
+            case "color":
+                $bookArray = sortByColor($bookArray);
+                break;
+            case "author":
+                $bookArray = sortByAuthor($bookArray);
+                break;
+        }
+    }
+}
+
 ?>
 
 <body>
@@ -13,7 +36,7 @@ require __DIR__ . "/header.php";
         <?php endforeach; ?>
     </div>
     <div class="buttons">
-        <form action="index.php" method="POST">
+        <form action="index.php" method="GET">
             <button name="sorting" type="submit" value="alphabetically">Alphabetically</button>
             <button name="sorting" type="submit" value="size">Size</button>
             <button name="sorting" type="submit" value="color">Colour</button>
