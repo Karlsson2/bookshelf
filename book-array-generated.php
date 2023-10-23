23712cdc6804f461e466a221dcd7a521ce2fb78e
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2023-10-23 10:40:07 +0200

    added generated file

---
+++ book-array-generated.php
@@ -0,0 +1,164 @@
+<?php
+
+$bookArray = array(
+    array(
+        "author" => "Johman John-Merwe",
+        "title" => "His Alien Song",
+        "ISBN" => "1436247151379",
+        "genre" => "Horror",
+        "page count" => 909,
+        "color" => "black"
+    ),
+    array(
+        "author" => "Danmah Thomtino",
+        "title" => "Their Macabre Innocence",
+        "ISBN" => "1096903811896",
+        "genre" => "Horror",
+        "page count" => 184,
+        "color" => "white"
+    ),
+    array(
+        "author" => "Kevliam Hansiams",
+        "title" => "Your Intimate Butcher",
+        "ISBN" => "1286645771489",
+        "genre" => "Thriller",
+        "page count" => 523,
+        "color" => "black"
+    ),
+    array(
+        "author" => "Marnifer Joniams",
+        "title" => "Inside The Opulent Song",
+        "ISBN" => "1053001570168",
+        "genre" => "Crime",
+        "page count" => 456,
+        "color" => "green"
+    ),
+    array(
+        "author" => "Benchan Davben",
+        "title" => "My Enchanting Innocence",
+        "ISBN" => "1573106165982",
+        "genre" => "Fantasy",
+        "page count" => 417,
+        "color" => "green"
+    ),
+    array(
+        "author" => "Lisicz Andesen",
+        "title" => "Inside The Chilling Affair",
+        "ISBN" => "1773404689493",
+        "genre" => "Fantasy",
+        "page count" => 798,
+        "color" => "pink"
+    ),
+    array(
+        "author" => "Ryalie Davoro",
+        "title" => "Her Refined Court",
+        "ISBN" => "1648876590911",
+        "genre" => "Romance",
+        "page count" => 703,
+        "color" => "white"
+    ),
+    array(
+        "author" => "Katicz Sundoro",
+        "title" => "Our Otherworldly Walk",
+        "ISBN" => "1021977686145",
+        "genre" => "Non-Fiction",
+        "page count" => 943,
+        "color" => "grey"
+    ),
+    array(
+        "author" => "Thoael Sundini",
+        "title" => "Our Elegant Circle",
+        "ISBN" => "1751547766042",
+        "genre" => "Fantasy",
+        "page count" => 587,
+        "color" => "green"
+    ),
+    array(
+        "author" => "Robric Smirström",
+        "title" => "My Enchanting Song",
+        "ISBN" => "1684289400624",
+        "genre" => "Science Fiction",
+        "page count" => 838,
+        "color" => "pink"
+    ),
+    array(
+        "author" => "Robric Orltiwa",
+        "title" => "Under The Bloody Seduction",
+        "ISBN" => "1678318292302",
+        "genre" => "Non-Fiction",
+        "page count" => 674,
+        "color" => "magenta"
+    ),
+    array(
+        "author" => "Johmah Willliu",
+        "title" => "Under The Elegant Orc",
+        "ISBN" => "1031274981377",
+        "genre" => "Crime",
+        "page count" => 524,
+        "color" => "magenta"
+    ),
+    array(
+        "author" => "Patchan Ivanov",
+        "title" => "Inside The Bloody Case",
+        "ISBN" => "1046180712411",
+        "genre" => "Romance",
+        "page count" => 825,
+        "color" => "black"
+    ),
+    array(
+        "author" => "Jenael Thomwon",
+        "title" => "A Second Macabre Business",
+        "ISBN" => "1809406120822",
+        "genre" => "Horror",
+        "page count" => 938,
+        "color" => "green"
+    ),
+    array(
+        "author" => "Wilchi Hussaran",
+        "title" => "Inside The Intimate Walk",
+        "ISBN" => "1052923307634",
+        "genre" => "Science Fiction",
+        "page count" => 800,
+        "color" => "green"
+    ),
+    array(
+        "author" => "Thodrzej Vandeer",
+        "title" => "Inside The Tender Grind",
+        "ISBN" => "1843516885660",
+        "genre" => "Fantasy",
+        "page count" => 476,
+        "color" => "pink"
+    ),
+    array(
+        "author" => "Tholie Marczan",
+        "title" => "Inside The Ineffable Court",
+        "ISBN" => "1854882535046",
+        "genre" => "Horror",
+        "page count" => 514,
+        "color" => "grey"
+    ),
+    array(
+        "author" => "Elimah Willtan",
+        "title" => "Another Bloody Gardener",
+        "ISBN" => "1811505364225",
+        "genre" => "Science Fiction",
+        "page count" => 353,
+        "color" => "blue"
+    ),
+    array(
+        "author" => "Johric Zhangwon",
+        "title" => "Inside The Bloody Lover",
+        "ISBN" => "1723627652463",
+        "genre" => "Thriller",
+        "page count" => 924,
+        "color" => "pink"
+    ),
+    array(
+        "author" => "Lischan Wuben",
+        "title" => "Under The Elegant Raven",
+        "ISBN" => "1176412904688",
+        "genre" => "Crime",
+        "page count" => 311,
+        "color" => "pink"
+    )
+);
